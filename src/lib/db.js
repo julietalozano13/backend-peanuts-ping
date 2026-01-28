46ce2ab57f73cd0fefb736f3b4427c78a2f4c271
julietalozano13 <julieta.lozano131004@gmail.com>
2026-01-28 19:07:41 +0900

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,12 +4,14 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
   try {
     const { MONGO_URI } = ENV;
-    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not set");
+    }
 
-    const conn = await mongoose.connect(ENV.MONGO_URI);
+    const conn = await mongoose.connect(MONGO_URI);
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
-    console.error("Error connection to MONGODB:", error);
-    process.exit(1); 
+    console.error("MongoDB connection error:", error.message);
+    throw error; 
   }
 };
